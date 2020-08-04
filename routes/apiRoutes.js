5503145ad186fdc14c1d110b8ffd4c572ce6b81c
chages16 <chages16@gmail.com>
2020-08-04 20:01:48 +0930

    Changes added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,49 @@
-const fs = require("fs")
-//const uuid = require("uuid")
-const { v4: uuid } = require('uuid')
-const path = require("path")
+var fs = require("fs");
+module.exports = function (app) {
+  // Setup notes variable
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
 
-//API for renderning  notes stored on db
-module.exports = function(app){
-app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "../db/db.json")))
+    // Store the contents of the db.json file in a variable for better performance
+    var notes = JSON.parse(data);
 
- //API for storing user added note and renderning updated  notes stored on db.json
- app.post("/api/notes", (req, res) => {
-     let newNote ={
-         //UUID generates unique id
-         id:uuid(),
-         title:req.body.title,
-         text:req.body.text
-     };
-     let oldNote =JSON.parse(fs.readFileSync(path.join(__dirname,"../db/db.json"),"utf-8")) 
-     oldNote.push(newNote)
-     fs.writeFileSync("./db/db.json",JSON.stringify(oldNote))
-     res.json(oldNote)
- })
+    // API ROUTES
+    // ========================================================
 
- 
- //Receive a query parameter containing the id of a note to delete.
- app.delete("/api/notes/:id", (req, res) => {
-     let choosen = req.params.id
-     let oldNote =JSON.parse(fs.readFileSync(path.join(__dirname,"../db/db.json"),"utf-8"))
-     const newNote =oldNote.filter(oldNote=>oldNote.id != choosen)
-     fs.writeFileSync("./db/db.json",JSON.stringify(newNote))
-     res.send(newNote)
- })
-}
+    // Setup the /api/notes get route
+    app.get("/api/notes", function (req, res) {
+      // Read the db.json file and return all saved notes as JSON.
+      res.json(notes);
+    });
+
+    // Setup the /api/notes post route
+    app.post("/api/notes", function (req, res) {
+      // Receives a new note, adds it to db.json, then returns the new note
+      let newNote = req.body;
+      notes.push(newNote);
+      updateDb();
+      return console.log("New Note Created " + newNote.title);
+    });
+
+    // Retrieves a note with specific id
+    app.get("/api/notes/:id", function (req, res) {
+      // display json for the notes array indices of the provided id
+      res.json(notes[req.params.id]);
+    });
+
+    // Deletes a note with specific id
+    app.delete("/api/notes/:id", function (req, res) {
+      notes.splice(req.params.id, 1);
+      updateDb();
+      console.log("Deleted note: " + req.params.id);
+    });
+
+    //updates the json file whenever a note is added or deleted
+    function updateDb() {
+      fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), (err) => {
+        if (err) throw err;
+        return true;
+      });
+    }
+  });
+};
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
+app.use(express.static("public"));
+
 
 
 // ================================================================================
